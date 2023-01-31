e133078805e2d78078fad0328141511944ee79ab
Selempo <32088026+kisio@users.noreply.github.com>
2023-01-31 10:24:57 +0300

    sets

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#seoqq^jm57020t#+_n$ig-y6)_fm#zgh&9pyhx!qkme^-0578'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
